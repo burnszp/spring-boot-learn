fdb3ac9ff5d2bf190f252b5f5e8b1e5a00e530ed
zhangfeng <zhangfeng@magorobot.cn>
2019-08-07 18:09:05 +0800

    Spring Boot 中的Json使用，Jackson注解详解

---
+++ spring-boot-security-base/src/main/java/com/friends/springbootsecuritybase/SpringBootSecurityBaseApplication.java
@@ -0,0 +1,12 @@
+package com.friends.springbootsecuritybase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityBaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityBaseApplication.class, args);
+    }
+
+}
