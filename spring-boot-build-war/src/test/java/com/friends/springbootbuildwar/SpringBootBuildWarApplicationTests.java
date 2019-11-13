a120cd9e1e4de4eb628d19e3fd15487e281ff6e1
zhangfeng <zhangfeng@magorobot.cn>
2019-11-13 13:50:26 +0800

    spring-boot-build-war

---
+++ spring-boot-build-war/src/test/java/com/friends/springbootbuildwar/SpringBootBuildWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friends.springbootbuildwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBuildWarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
